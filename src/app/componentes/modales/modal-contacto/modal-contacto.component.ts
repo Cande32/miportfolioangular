f401d2549e40f7b830ed9ce165147bf27f0eeb20
Cande32 <ferreyracandela@gmail.com>
2023-05-18 18:39:23 -0300

    modifico modal de contacto

--- src/app/componentes/modales/modal-contacto/modal-contacto.component.ts
+++ src/app/componentes/modales/modal-contacto/modal-contacto.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-modal-contacto',
   templateUrl: './modal-contacto.component.html',
   styleUrls: ['./modal-contacto.component.css']
 })
-export class ModalContactoComponent {
+export class ModalContactoComponent implements OnInit{
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder){
+    this.form= this.formBuilder.group({
+      fullname:['', Validators.required],
+      email:['', [Validators.required, Validators.email]],
+      mensaje:['', [Validators.required, Validators.maxLength(500)]],
+   })
+  }
+
+ngOnInit() {}
+
+
+get Mail(){
+ return this.form.get("email");
+}
+
+get MailValid() {
+  return this.Mail?.touched && !this.Mail?.valid;
+}
+
+onEnviar(event: Event){
+ 
+  event.preventDefault; 
+
+  if (this.form.valid){
+    alert("¡Tu mensaje fue enviado!")
+    
+  }else{
+       
+    this.form.markAllAsTouched();
+    alert("¡Revisar formulario!") 
+  }
+}
 
 }
+
--- src/app/componentes/modales/modal-logout/modal-logout.component.ts
+++ src/app/componentes/modales/modal-logout/modal-logout.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-modal-logout',
   templateUrl: './modal-logout.component.html',
